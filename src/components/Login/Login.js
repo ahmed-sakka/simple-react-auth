65b76be081528c23b174b8550ed2ddd5ea689071
Ahmed Sakka <ahmedsakka720@gmail.com>
2023-03-01 11:47:21 +0100

    focus on the first invalid input using useRef and useImperativeHandle hooks

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useReducer, useContext } from 'react';
+import React, {
+  useState,
+  useEffect,
+  useReducer,
+  useContext,
+  useRef,
+} from 'react';
 
 import classes from './Login.module.css';
 import Card from '../UI/Card/Card';
@@ -28,6 +34,9 @@ const passwordReducer = (state, action) => {
 
 const Login = (props) => {
   const authCtx = useContext(AuthContext);
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
   const [formIsValid, setFormIsValid] = useState(false);
 
@@ -83,7 +92,13 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value);
+    if (formIsValid) {
+      authCtx.onLogin(emailState.value, passwordState.value);
+    } else if (!emailIsValid) {
+      emailRef.current.focus();
+    } else {
+      passwordRef.current.focus();
+    }
   };
 
   return (
@@ -97,6 +112,7 @@ const Login = (props) => {
           isValid={emailIsValid}
           onChange={emailChangeHandler}
           onBlur={validateEmailHandler}
+          ref={emailRef}
         />
         <Input
           id='password'
@@ -106,9 +122,10 @@ const Login = (props) => {
           isValid={passwordIsValid}
           onChange={passwordChangeHandler}
           onBlur={validatePasswordHandler}
+          ref={passwordRef}
         />
         <div className={classes.actions}>
-          <Button type='submit' className={classes.btn} disabled={!formIsValid}>
+          <Button type='submit' className={classes.btn}>
             Login
           </Button>
         </div>
